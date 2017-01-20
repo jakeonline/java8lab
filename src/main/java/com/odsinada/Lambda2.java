c0014e6172cde23415c0614474ae22a28f514dd7
Jacob Odsinada <jacob@odsinada.com>
2017-01-21 00:05:53 +0800

    Adding Lambda2

---
+++ src/main/java/com/odsinada/Lambda2.java
@@ -0,0 +1,12 @@
+package com.odsinada;
+
+import java.util.Arrays;
+
+public class Lambda2 {
+
+    public static void main(String args[]) {
+
+        System.out.println("hello world!");
+
+    }
+}
