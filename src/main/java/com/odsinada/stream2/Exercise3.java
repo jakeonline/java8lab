f194b255941971a0ea56c1f2cedaced55b38cb5b
Jacob Odsinada <jacob@odsinada.com>
2018-05-16 00:43:06 +1000

    Fixed Streams 2 exercises

--- src/main/java/com/odsinada/stream2/Exercise3.java
+++ src/main/java/com/odsinada/stream2/Exercise3.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringJoiner;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Exercise3 {
@@ -14,9 +15,7 @@ public class Exercise3 {
 
         List<String> words = Arrays.asList("hi", "hello", "how", "are", "you");
 
-        StringJoiner sj = new StringJoiner(",");
-
-        System.out.println(words.stream().reduce((s1, s2) -> s1.concat(",").concat(s2)).get());
+        System.out.println(words.stream().reduce((s1, s2) -> String.join(",", s1, s2)).get());
 
         // extra
         int[] numbers = new int[3];
@@ -26,13 +25,21 @@ public class Exercise3 {
 
         System.out.println(Arrays.stream(numbers).mapToObj((num) -> "" + num)
                 .map(s -> "'".concat(s).concat("'"))
-                .reduce((s1, s2) -> s1.concat(",").concat(s2)).get());
+                .reduce((s1, s2) -> String.join(",", s1, s2)).get());
+        System.out.println(Arrays.stream(numbers).mapToObj((num) -> "" + num)
+                .map(s -> "'".concat(s).concat("'"))
+                .collect(Collectors.joining(",")));
 
         List<Integer> numbs = new ArrayList<>();
         numbs.add(4444);
         numbs.add(5555);
         numbs.add(6666);
-        System.out.println(Stream.of(numbs).map(s -> s.toString()).reduce((s1, s2) -> s1.concat(",").concat(s2)).get());
+        System.out.println(Stream.of(numbs)
+                .map(s -> s.toString())
+                .reduce((s1, s2) -> String.join(",", s1, s2)).get());
+        System.out.println(Stream.of(numbs)
+                .map(s -> s.toString())
+                .collect(Collectors.joining(",")));
     }
 
 }
